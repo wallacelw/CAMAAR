72c9ea3208d1437c317f62c0d03e1d4b9111f830
Wallace <wallacebtlw@gmail.com>
2024-06-16 21:02:15 -0300

    navbar fixed, turmas view, read json

--- CAAMAR/config/routes.rb
+++ CAAMAR/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'turmas/index'
   root 'auth#login'
   get 'main/index'
   get 'gerenciamento', to: 'gerenciamento#show'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
