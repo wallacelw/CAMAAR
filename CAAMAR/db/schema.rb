02b4e1aba71fbb765298d2b13046510b5532ecb6
Henrique de Oliveira Ramos <henriqueramos.qs@gmail.com>
2024-06-21 18:12:01 -0300

    Antes de fazer algo arriscado

--- CAAMAR/db/schema.rb
+++ CAAMAR/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_19_234942) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_21_201135) do
   create_table "usuarios", force: :cascade do |t|
     t.string "matricula"
     t.string "nome"
@@ -19,6 +19,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_19_234942) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "senha"
+    t.boolean "isAdmin"
+    t.boolean "isAluno"
+    t.boolean "isProf"
+    t.string "Curso"
+    t.string "departamento"
   end
 
 end
