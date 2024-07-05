124de6724f13c85148a6ebcd09551617f54dc0a2
luisf <luisfelipehcarmona@gmail.com>
2024-07-05 11:34:30 -0300

    nova visualização das estatisticas

--- CAAMAR/app/controllers/forms_statiscis_controller.rb
+++ CAAMAR/app/controllers/forms_statiscis_controller.rb
@@ -1,7 +1,6 @@
 class FormsStatiscisController < ApplicationController
   def index
-    @answers = JSON.parse(File.read("db/json/answers.json"))
-    @forms = JSON.parse(File.read("db/json/forms.json"))
+    @forms = JSON.parse(File.read("C:/Users/luisf/Documents/Eng Software/study/Test/db/json/forms.json"))
   end
 
 
